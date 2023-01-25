721302b3df430ddac88d51f738de7628ddd5256a
MakotoMiyamoto <makotonomeru@gmail.com>
2023-01-25 00:52:56 -0600

    Move merge_sort to module

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,11 @@
-use std::{fmt::Debug, collections::VecDeque};
+mod merge_sort;
 
-fn merge_sort<T: Copy + PartialOrd + Debug>(a: Vec<T>) -> Vec<T> {
-    if a.len() <= 1 {
-        return a;
-    }
-
-    let left = merge_sort(a[..a.len()/2].to_vec());
-    let right = merge_sort(a[a.len()/2..].to_vec());
-
-    merge(left, right)
-}
-
-fn merge<T: Copy + PartialOrd + Debug>(left: Vec<T>, right: Vec<T>) -> Vec<T> {
-    println!("{left:?} , {right:?}");
-    let mut rs: Vec<T> = vec![];
-    let mut left = VecDeque::from(left);
-    let mut right = VecDeque::from(right);
-
-    while !left.is_empty() && !right.is_empty() {
-        if left.front().unwrap() < right.front().unwrap() {
-            rs.push(left.pop_front().unwrap());
-        }
-        else {
-            rs.push(right.pop_front().unwrap());
-        }
-    }
-
-    while !left.is_empty() {
-        rs.push(left.pop_front().unwrap());
-    }
-    while !right.is_empty() {
-        rs.push(right.pop_front().unwrap());
-    }
-    
-    rs
-}
+use merge_sort::merge_sort;
 
 fn main() {
     let v = vec![5, 2, 3, 7, 8, 4, 6, 9, 1];
 
+    println!("---- Merge Sort ----");
     println!("{:?}", merge_sort(v));
+    println!("--- End of Merge ---");
 }
